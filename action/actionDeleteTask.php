09699ec8d6f53be0e1468279c78f3ce6c682c17a
ser1372 <ser1372@ukr.net>
2022-12-27 19:32:56 +0200

    first commit

---
+++ action/actionDeleteTask.php
@@ -0,0 +1,10 @@
+<?php
+// delete task
+if (isset($_GET['del_task'])) {
+	$id = $_GET['del_task'];
+
+	mysqli_query($db, "DELETE FROM tasks WHERE id=".$id);
+	header('location: index.php');
+}
+
+?>
